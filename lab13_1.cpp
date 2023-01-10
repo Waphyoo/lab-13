02ec9d05f367c9cec257e248e907605952b1785c
n@gmail.com <n@gmail.com>
2023-01-10 15:15:45 +0700

    okok

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
 
@@ -10,10 +11,12 @@ int fibonacci(int x);
 
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
 
